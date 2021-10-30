334f182ab1159f2bacd7204450b0fcdd41cb8ebc
jinho <g1063114@naver.com>
2021-10-30 21:54:04 +0900

    posts entity

---
+++ src/main/java/com/jeeen/webservice/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.jeeen.webservice.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT",nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder    // setter 메서드로 설정하는 것보다 builder 패턴을 사용해서 값 세팅
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}

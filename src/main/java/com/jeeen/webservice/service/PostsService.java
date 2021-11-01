f5bfa40a1a4127e0ee5b2cae71751fc1adfa0edf
jinho <g1063114@naver.com>
2021-11-01 13:12:27 +0900

    jpa posts update, findById test

--- src/main/java/com/jeeen/webservice/service/PostsService.java
+++ src/main/java/com/jeeen/webservice/service/PostsService.java
@@ -1,10 +1,15 @@
 package com.jeeen.webservice.service;
 
+import com.jeeen.webservice.domain.posts.Posts;
 import com.jeeen.webservice.domain.posts.PostsRepository;
+import com.jeeen.webservice.web.dto.PostsResponseDto;
 import com.jeeen.webservice.web.dto.PostsSaveRequestDto;
+import com.jeeen.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +19,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id)
+        );
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id)
+        );
+        return new PostsResponseDto(entity);
     }
 }
--- src/test/java/com/jeeen/webservice/web/PostsApiControllerTest.java
+++ src/test/java/com/jeeen/webservice/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.jeeen.webservice.web;
 import com.jeeen.webservice.domain.posts.Posts;
 import com.jeeen.webservice.domain.posts.PostsRepository;
 import com.jeeen.webservice.web.dto.PostsSaveRequestDto;
+import com.jeeen.webservice.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -63,5 +66,41 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
         assertThat(all.get(0).getAuthor()).isEqualTo(author);
+    }
+
+    @Test
+    public void 수정() throws Exception{
+        // given
+        Posts save = postsRepository.save(
+                Posts.builder()
+                        .title("title")
+                        .content("content")
+                        .author("author")
+                        .build()
+        );
+
+        Long updateId = save.getId();
+        String changeTitle = "title2";
+        String changeContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(changeTitle)
+                .content(changeContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(changeTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(changeContent);
     }
 }

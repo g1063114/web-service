29eaba461c3faa3f8dadebca2574371aad682260
jinho <g1063114@naver.com>
2021-11-02 13:57:27 +0900

    jpa auditing

--- src/test/java/com/jeeen/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jeeen/webservice/domain/posts/PostsRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -44,5 +46,24 @@ public class PostsRepositoryTest {
         assertThat(postsList.get(0).getTitle()).isEqualTo(title);
         assertThat(postsList.get(0).getContent()).isEqualTo(content);
         assertThat(postsList.get(0).getAuthor()).isEqualTo(author);
+    }
+
+    @Test
+    public void BasTime_등록(){
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> list = postsRepository.findAll();
+
+        // then
+        Posts posts = list.get(0);
+        System.out.println("posts.getCreatedDate() = " + posts.getCreatedDate());
+        System.out.println("posts.getModifiedDate() = " + posts.getModifiedDate());
     }
 }

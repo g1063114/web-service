7220d1eada7350701d73364675ffebb3cecd72c0
jinho <g1063114@naver.com>
2021-11-08 19:14:31 +0900

    posts update

--- src/main/java/com/jeeen/webservice/controller/PostsPageController.java
+++ src/main/java/com/jeeen/webservice/controller/PostsPageController.java
@@ -4,13 +4,13 @@ import com.jeeen.webservice.domain.posts.Posts;
 import com.jeeen.webservice.service.PostsService;
 import com.jeeen.webservice.web.dto.PostsFormDto;
 import com.jeeen.webservice.web.dto.PostsListResponseDto;
+import com.jeeen.webservice.web.dto.PostsResponseDto;
+import com.jeeen.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -44,4 +44,21 @@ public class PostsPageController {
         return "posts/postsList";
     }
 
+    @GetMapping("/posts/{id}/edit")
+    public String editForm(@PathVariable("id") Long id, Model model){
+        PostsResponseDto responseDto = postsService.findById(id);
+        model.addAttribute("form", responseDto);
+        return "posts/editForm";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String edit(@PathVariable("id") Long id, @ModelAttribute("form") PostsUpdateRequestDto form){
+        PostsUpdateRequestDto updateDto = PostsUpdateRequestDto.builder()
+                .title(form.getTitle())
+                .content(form.getContent())
+                .build();
+
+        postsService.update(id,updateDto);
+        return "redirect:/posts";
+    }
 }
--- src/main/java/com/jeeen/webservice/web/dto/PostsResponseDto.java
+++ src/main/java/com/jeeen/webservice/web/dto/PostsResponseDto.java
@@ -2,8 +2,10 @@ package com.jeeen.webservice.web.dto;
 
 import com.jeeen.webservice.domain.posts.Posts;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class PostsResponseDto {
 
     private Long id;

4b6be93fbbc9a27373d773cea3d2301596124976
jinho <g1063114@naver.com>
2021-11-09 18:23:15 +0900

    user login, logout

--- src/main/java/com/jeeen/webservice/web/IndexController.java
+++ src/main/java/com/jeeen/webservice/web/IndexController.java
@@ -1,14 +1,27 @@
 package com.jeeen.webservice.web;
 
+import com.jeeen.webservice.config.auth.dto.SessionUser;
+import com.jeeen.webservice.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if( user != null ){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 }
